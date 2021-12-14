61c5c0cb5ffa93d2b7b356dd26c6b2657c38f43e
anan416 <90143779+anan416@users.noreply.github.com>
2021-12-14 16:56:42 -0300

    Add files via upload

---
+++ cod.js
@@ -0,0 +1,8 @@
+function mostrar(){
+    if (document.querySelector(".nuevo_nav").style.display == "block"){
+        document.querySelector(".nuevo_nav").style.display = "none";
+    }
+    else{
+        document.querySelector(".nuevo_nav").style.display = "block";
+    }
+}
