2ad06e56d2fd43e406e55679ed72626b9b8b62da
anan416 <90143779+anan416@users.noreply.github.com>
2022-01-17 10:45:16 -0300

    Add files via upload

--- cod.js
+++ cod.js
@@ -1,3 +1,11 @@
+yo = document.getElementById("sobreMi");
+conoc = document.getElementById("conocimientos");
+cur = document.getElementById("cursos");
+pro = document.getElementById("proyectos");
+cont = document.getElementById("contactame");
+footer = document.querySelector("footer");
+
+
 function mostrar(){
     if (document.querySelector(".nuevo_nav").style.display == "block"){
         document.querySelector(".nuevo_nav").style.display = "none";
@@ -5,4 +13,49 @@ function mostrar(){
     else{
         document.querySelector(".nuevo_nav").style.display = "block";
     }
+}
+
+function sobreMi(){
+    yo.style.display = "block";
+    conoc.style.display = "none";
+    cur.style.display = "none";
+    pro.style.display = "none";
+    cont.style.display = "none";
+    footer.style.display = "block";
+}
+
+function conocimientos(){
+    yo.style.display = "none";
+    conoc.style.display = "block";
+    cur.style.display = "none";
+    pro.style.display = "none";
+    cont.style.display = "none";
+    footer.style.display = "block";
+}
+
+function cursos(){
+    yo.style.display = "none";
+    conoc.style.display = "none";
+    cur.style.display = "block";
+    pro.style.display = "none";
+    cont.style.display = "none";
+    footer.style.display = "block";
+}
+
+function proyectos(){
+    yo.style.display = "none";
+    conoc.style.display = "none";
+    cur.style.display = "none";
+    pro.style.display = "block";
+    cont.style.display = "none";
+    footer.style.display = "block";
+}
+
+function contactame(){
+    yo.style.display = "none";
+    conoc.style.display = "none";
+    cur.style.display = "none";
+    pro.style.display = "none";
+    cont.style.display = "block";
+    footer.style.display = "block";
 }
